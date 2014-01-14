8fa53d0d3fbc9d813070a2c45cef2506c3c715ea
Matthew Davey <matthew.davey@dotbunny.com>
2014-01-13 21:14:43 -0500

    More documentation.

--- Examples/Other/Jobs/MeshCombinerExample.cs
+++ Examples/Other/Jobs/MeshCombinerExample.cs
@@ -7,21 +7,19 @@ public class MeshCombinerExample : MonoBehaviour
 {
 		Hydrogen.Threading.Jobs.MeshCombiner _meshCombiner;
 		public Transform TargetMeshes;
-		//public Material defaultMaterial;
-		// Update is called once per frame
-		void LateUpdate ()
-		{
-				if (_meshCombiner != null) {
-						_meshCombiner.Check ();
-				}
-		}
 
-		public IEnumerator ProcessMeshFilters ()
+		/// <summary>
+		/// Process meshFilters in Unity's main thread, as we are required to by Unity. At least we've rigged it as a 
+		/// coroutine! Right? OK I know I really wish we could have used mesh data in a thread but properties die as well.
+		/// </summary>
+		/// <returns>IEnumartor aka Coroutine</returns>
+		/// <remarks>
+		/// For the sake of the demo we are going to need to roll over the "Target" to find all the 
+		/// meshes that we need to look at, but in theory you could do this without having to load the
+		/// object by simply having raw mesh data, or any other means of accessing it.
+		/// </remarks>
+		public IEnumerator PreProcess ()
 		{
-				// For the sake of the demo we are going to need to roll over the "Target" to find all the 
-				// meshes that we need to look at, but in theory you could do this without having to load the
-				// object by simply having raw mesh data, or any other means of accessing it.
-
 				// Create a new MeshCombiner (we dont want any old data kicking around)
 				_meshCombiner = new Hydrogen.Threading.Jobs.MeshCombiner ();
 
@@ -45,13 +43,20 @@ public class MeshCombinerExample : MonoBehaviour
 				}
 
 				// Start the threaded love
-				_meshCombiner.Combine (System.Threading.ThreadPriority.Normal, null, ProcessMeshesInsideUnity);
+				_meshCombiner.Combine (System.Threading.ThreadPriority.Normal, null, ThreadCallback);
 		}
 
-		public IEnumerator ProcessMeshes (int hash, 
-		                                  Hydrogen.Threading.Jobs.MeshCombiner.MeshDescription[] meshDescriptions, 
-		                                  Material[] materials)
+		/// <summary>
+		/// Process the MeshDescription data sent back from the Combiner and make it appear!
+		/// </summary>
+		/// <param name="hash">Instance Hash.</param>
+		/// <param name="meshDescriptions">MeshDescriptions.</param>
+		/// <param name="materials">Materials.</param>
+		public IEnumerator PostProcess (int hash, 
+		                                Hydrogen.Threading.Jobs.MeshCombiner.MeshDescription[] meshDescriptions, 
+		                                Material[] materials)
 		{
+				// Create our dummy list of meshes
 				var meshes = new List<Mesh> ();
 
 
@@ -65,14 +70,14 @@ public class MeshCombinerExample : MonoBehaviour
 						yield return new WaitForEndOfFrame ();
 				}
 
-				GameObject go = new GameObject ("Combined Meshes");
+				var go = new GameObject ("Combined Meshes");
 				go.transform.position = TargetMeshes.position;
 				go.transform.rotation = TargetMeshes.rotation;
 
 				// Show Them
 				for (int y = 0; y <= meshes.Count; y++) {
-						GameObject meshObject = new GameObject ();
-						meshObject.name = meshes [y].name;
+						var meshObject = new GameObject ();
+						meshObject.name = hash + "_" + meshes [y].name;
 						meshObject.transform.parent = go.transform;
 						meshObject.transform.position = Vector3.zero;
 						meshObject.transform.rotation = Quaternion.identity;
@@ -85,24 +90,50 @@ public class MeshCombinerExample : MonoBehaviour
 				_meshCombiner = null;
 		}
 
-		public void ProcessMeshesInsideUnity (int hash, Hydrogen.Threading.Jobs.MeshCombiner.MeshDescription[] meshDescriptions, Material[] materials)
+		/// <summary>
+		/// This function is called in the example after the MeshCombiner has processed the meshes, it starts a Coroutine 
+		/// to create the actual meshes based on the flat data. This is the most optimal way to do this sadly as we cannot
+		/// create or touch Unity based meshes outside of the main thread.
+		/// </summary>
+		/// <param name="hash">Instance Hash.</param>
+		/// <param name="meshDescriptions">MeshDescriptions.</param>
+		/// <param name="materials">Materials.</param>
+		public void ThreadCallback (int hash, Hydrogen.Threading.Jobs.MeshCombiner.MeshDescription[] meshDescriptions, Material[] materials)
 		{
 				// This is just a dirty way to see if we can squeeze jsut a bit more performance out of Unity when 
 				// making all of the meshes for us (instead of it being done in one call, we use a coroutine with a loop.
-				StartCoroutine (ProcessMeshes (hash, meshDescriptions, materials));
+				StartCoroutine (PostProcess (hash, meshDescriptions, materials));
 		}
 
+		/// <summary>
+		/// Unity's LateUpdate Event
+		/// </summary>
+		void LateUpdate ()
+		{
+				// If we have a MeshCombiner lets run the Check()
+				if (_meshCombiner != null) {
+						// Funny thing about this method of doing this; lots of Thread based solutions in Unity have an
+						// elaborate manager that does this for you ... just saying.
+						_meshCombiner.Check ();
+				}
+		}
+
+		/// <summary>
+		/// Unity's OnGUI Event
+		/// </summary>
 		void OnGUI ()
 		{
-
+				// A clever little trick to only show the button when nothing is going on.
+				// Obviously, in a real world setting this would probably not look like this.
 				if (_meshCombiner == null) {
 
 						if (GUI.Button (new Rect (5, 5, 200, 35), "Rock & || Possibly Roll!")) {
-								StartCoroutine (ProcessMeshFilters ());
+								StartCoroutine (PreProcess ());
 						}
 
 				}
 
+				// Some debug output, helpful for determining if we block the main thread much.
 				GUI.color = Color.black;
 				GUI.Label (new Rect (210, 12, 100, 35), Time.time.ToString ());
 		}
