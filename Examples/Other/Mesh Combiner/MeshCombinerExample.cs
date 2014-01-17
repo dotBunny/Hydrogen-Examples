aa533d6e398f60a4592613638df0687f8d3aac62
Matthew Davey <matthew.davey@dotbunny.com>
2014-01-17 14:19:34 -0500

    Working Example - Just not weird meshes.

--- Examples/Other/Mesh Combiner/MeshCombinerExample.cs
+++ Examples/Other/Mesh Combiner/MeshCombinerExample.cs
@@ -5,132 +5,134 @@ using System.Collections.Generic;
 
 public class MeshCombinerExample : MonoBehaviour
 {
-		Hydrogen.Threading.Jobs.MeshCombiner _meshCombiner = new Hydrogen.Threading.Jobs.MeshCombiner ();
-		public Transform TargetMeshes;
+	Hydrogen.Threading.Jobs.MeshCombiner _meshCombiner = new Hydrogen.Threading.Jobs.MeshCombiner ();
+	public Transform TargetMeshes;
 
-		/// <summary>
-		/// Process meshFilters in Unity's main thread, as we are required to by Unity. At least we've rigged it as a 
-		/// coroutine! Right? OK I know I really wish we could have used mesh data in a thread but properties die as well.
-		/// </summary>
-		/// <returns>IEnumartor aka Coroutine</returns>
-		/// <remarks>
-		/// For the sake of the demo we are going to need to roll over the "Target" to find all the 
-		/// meshes that we need to look at, but in theory you could do this without having to load the
-		/// object by simply having raw mesh data, or any other means of accessing it.
-		/// </remarks>
-		public IEnumerator PreProcess ()
-		{
-				// Create a new MeshCombiner (we dont want any old data kicking around)
-				_meshCombiner = new Hydrogen.Threading.Jobs.MeshCombiner ();
+	/// <summary>
+	/// Process meshFilters in Unity's main thread, as we are required to by Unity. At least we've rigged it as a 
+	/// coroutine! Right? OK I know I really wish we could have used mesh data in a thread but properties die as well.
+	/// </summary>
+	/// <returns>IEnumartor aka Coroutine</returns>
+	/// <remarks>
+	/// For the sake of the demo we are going to need to roll over the "Target" to find all the 
+	/// meshes that we need to look at, but in theory you could do this without having to load the
+	/// object by simply having raw mesh data, or any other means of accessing it.
+	/// </remarks>
+	public IEnumerator PreProcess ()
+	{
+		// Create a new MeshCombiner (we dont want any old data kicking around)
+		_meshCombiner = new Hydrogen.Threading.Jobs.MeshCombiner ();
 
-				// Yes We Hate This - There Are Better Implementations
-				MeshFilter[] meshFilters = TargetMeshes.GetComponentsInChildren<MeshFilter> ();
+		// Yes We Hate This - There Are Better Implementations
+		MeshFilter[] meshFilters = TargetMeshes.GetComponentsInChildren<MeshFilter> ();
 
-				// Need to leave this one in as a weird access race happens if you dont.
-				yield return new WaitForEndOfFrame ();
+		// Need to leave this one in as a weird access race happens if you dont.
+		yield return new WaitForEndOfFrame ();
 
-				// Loop through all of our mesh filters and add them to the combiner to be combined.
+		// Loop through all of our mesh filters and add them to the combiner to be combined.
 
-				for (int x = 0; x < meshFilters.Length; x++) {
+		for (int x = 0; x < meshFilters.Length; x++) {
 
-						if (meshFilters [x].gameObject.activeSelf) {
-								_meshCombiner.AddMesh (meshFilters [x], meshFilters [x].renderer, meshFilters [x].transform);
+			if (meshFilters [x].gameObject.activeSelf) {
+				_meshCombiner.AddMesh (meshFilters [x], 
+					meshFilters [x].renderer, 
+					meshFilters [x].transform.localToWorldMatrix);
+			}
 
-						}
+			meshFilters [x].gameObject.SetActive (false);
 
-						meshFilters [x].gameObject.SetActive (false);
-
-						// NOTE: This is what slows it down, but allows you to see the disassembly currently.
-						//yield return new WaitForEndOfFrame ();
-				}
-
-
-				// Start the threaded love
-				if (_meshCombiner.MeshInputCount > 0) {
-						_meshCombiner.Combine (ThreadCallback);
-				}
-				yield return new WaitForEndOfFrame ();
+			// NOTE: This is what slows it down, but allows you to see the disassembly currently.
+			//yield return new WaitForEndOfFrame ();
 		}
 
-		/// <summary>
-		/// Process the MeshDescription data sent back from the Combiner and make it appear!
-		/// </summary>
-		/// <param name="hash">Instance Hash.</param>
-		/// <param name="meshDescriptions">MeshDescriptions.</param>
-		/// <param name="materials">Materials.</param>
-		public IEnumerator PostProcess (int hash, Hydrogen.Threading.Jobs.MeshCombiner.MeshOutput[] meshOutputs)
-		{
-				var go = new GameObject ("Combined Meshes");
-				go.transform.position = TargetMeshes.position;
-				go.transform.rotation = TargetMeshes.rotation;
 
-				// Make our meshes in Unity
-				for (int x = 0; x < meshOutputs.Length; x++) {
-						var meshObject = new GameObject ();
+		// Start the threaded love
+		if (_meshCombiner.MeshInputCount > 0) {
+			_meshCombiner.Combine (ThreadCallback);
+		}
+		yield return new WaitForEndOfFrame ();
+	}
 
-						var newMesh = _meshCombiner.CreateMeshObject (meshOutputs [x]);
+	/// <summary>
+	/// Process the MeshDescription data sent back from the Combiner and make it appear!
+	/// </summary>
+	/// <param name="hash">Instance Hash.</param>
+	/// <param name="meshDescriptions">MeshDescriptions.</param>
+	/// <param name="materials">Materials.</param>
+	public IEnumerator PostProcess (int hash, Hydrogen.Threading.Jobs.MeshCombiner.MeshOutput[] meshOutputs)
+	{
+		var go = new GameObject ("Combined Meshes");
+		go.transform.position = TargetMeshes.position;
+		go.transform.rotation = TargetMeshes.rotation;
+	
+		// Make our meshes in Unity
+		for (int x = 0; x < meshOutputs.Length; x++) {
+			var meshObject = new GameObject ();
 
-						meshObject.name = "Mesh_" + hash + "_" + newMesh.GetHashCode ();
-						meshObject.transform.parent = go.transform;
-						meshObject.transform.position = Vector3.zero;
-						meshObject.transform.rotation = Quaternion.identity;
-						meshObject.AddComponent<MeshFilter> ().sharedMesh = newMesh.Mesh;
-						meshObject.AddComponent<MeshRenderer> ().sharedMaterials = newMesh.Materials;
+			var newMesh = _meshCombiner.CreateMeshObject (meshOutputs [x]);
 
-						// Fake Unity Threading
-						yield return new WaitForEndOfFrame ();
-				}
+			meshObject.name = "Mesh_" + hash + "_" + newMesh.GetHashCode ();
+			meshObject.AddComponent<MeshFilter> ().sharedMesh = newMesh.Mesh;
+			meshObject.AddComponent<MeshRenderer> ().sharedMaterials = newMesh.Materials;
+			meshObject.transform.parent = go.transform;
+			meshObject.transform.position = Vector3.zero;
+			meshObject.transform.rotation = Quaternion.identity;
 
-				// Clear previous data (for demonstration purposes)
-				// It could be useful to keep some mesh data in already parsed, then you could use the RemoveMesh function
-				// to remove ones that you want changed, without having to reparse mesh data.
-				_meshCombiner.ClearMeshes ();
+
+			// Fake Unity Threading
+			yield return new WaitForEndOfFrame ();
 		}
 
-		/// <summary>
-		/// This function is called in the example after the MeshCombiner has processed the meshes, it starts a Coroutine 
-		/// to create the actual meshes based on the flat data. This is the most optimal way to do this sadly as we cannot
-		/// create or touch Unity based meshes outside of the main thread.
-		/// </summary>
-		/// <param name="hash">Instance Hash.</param>
-		/// <param name="meshOutputs">.</param>
-		public void ThreadCallback (int hash, Hydrogen.Threading.Jobs.MeshCombiner.MeshOutput[] meshOutputs)
-		{
-				// This is just a dirty way to see if we can squeeze jsut a bit more performance out of Unity when 
-				// making all of the meshes for us (instead of it being done in one call, we use a coroutine with a loop.
-				StartCoroutine (PostProcess (hash, meshOutputs));
+		// Clear previous data (for demonstration purposes)
+		// It could be useful to keep some mesh data in already parsed, then you could use the RemoveMesh function
+		// to remove ones that you want changed, without having to reparse mesh data.
+		_meshCombiner.ClearMeshes ();
+	}
+
+	/// <summary>
+	/// This function is called in the example after the MeshCombiner has processed the meshes, it starts a Coroutine 
+	/// to create the actual meshes based on the flat data. This is the most optimal way to do this sadly as we cannot
+	/// create or touch Unity based meshes outside of the main thread.
+	/// </summary>
+	/// <param name="hash">Instance Hash.</param>
+	/// <param name="meshOutputs">.</param>
+	public void ThreadCallback (int hash, Hydrogen.Threading.Jobs.MeshCombiner.MeshOutput[] meshOutputs)
+	{
+		// This is just a dirty way to see if we can squeeze jsut a bit more performance out of Unity when 
+		// making all of the meshes for us (instead of it being done in one call, we use a coroutine with a loop.
+		StartCoroutine (PostProcess (hash, meshOutputs));
+	}
+
+	/// <summary>
+	/// Unity's LateUpdate Event
+	/// </summary>
+	void LateUpdate ()
+	{
+		// If we have a MeshCombiner lets run the Check()
+		if (_meshCombiner != null) {
+			// Funny thing about this method of doing this; lots of Thread based solutions in Unity have an
+			// elaborate manager that does this for you ... just saying.
+			_meshCombiner.Check ();
+		}
+	}
+
+	/// <summary>
+	/// Unity's OnGUI Event
+	/// </summary>
+	void OnGUI ()
+	{
+		// A clever little trick to only show the button when nothing is going on.
+		// Obviously, in a real world setting this would probably not look like this.
+		if (_meshCombiner != null && _meshCombiner.MeshInputCount == 0) {
+
+			if (GUI.Button (new Rect (5, 5, 200, 35), "Rock & || Possibly Roll!")) {
+				StartCoroutine (PreProcess ());
+			}
+
 		}
 
-		/// <summary>
-		/// Unity's LateUpdate Event
-		/// </summary>
-		void LateUpdate ()
-		{
-				// If we have a MeshCombiner lets run the Check()
-				if (_meshCombiner != null) {
-						// Funny thing about this method of doing this; lots of Thread based solutions in Unity have an
-						// elaborate manager that does this for you ... just saying.
-						_meshCombiner.Check ();
-				}
-		}
-
-		/// <summary>
-		/// Unity's OnGUI Event
-		/// </summary>
-		void OnGUI ()
-		{
-				// A clever little trick to only show the button when nothing is going on.
-				// Obviously, in a real world setting this would probably not look like this.
-				if (_meshCombiner != null && _meshCombiner.MeshInputCount == 0) {
-
-						if (GUI.Button (new Rect (5, 5, 200, 35), "Rock & || Possibly Roll!")) {
-								StartCoroutine (PreProcess ());
-						}
-
-				}
-
-				// Some debug output, helpful for determining if we block the main thread much.
-				GUI.color = Color.black;
-				GUI.Label (new Rect (210, 12, 100, 35), Time.time.ToString ());
-		}
+		// Some debug output, helpful for determining if we block the main thread much.
+		GUI.color = Color.black;
+		GUI.Label (new Rect (210, 12, 100, 35), Time.time.ToString ());
+	}
 }
